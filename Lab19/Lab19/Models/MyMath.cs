3c9f12d10801fd9889a42e97b11b3fc25421d762
GlebBrykin <glebsbrykin@gmail.com>
2024-11-21 18:31:56 +0300

    Add files via upload

--- Lab19/Lab19/Models/MyMath.cs
+++ Lab19/Lab19/Models/MyMath.cs
@@ -1,1 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Lab19.Models
+{
+
+    public static class MyMath
+    {
+
+        public static double CalcLengthUsingRadius(double radius)
+        {
+            return 2 * radius * Math.PI;
+        }
+
+    }
+
+}
